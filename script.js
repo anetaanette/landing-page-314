4ea2fa7270581d9a413cffe9707b47f85ed0f42d
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-11-28 11:43:02 +0100

    mobile almost done

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const splide = new Splide('.splide', {
     type: 'slide', //slide, loop, fade
     // padding: { y: 10 }, //css values
-    // gap: '20px', //1 rem default
-    // rewind: true,
+    gap: '65px', //1 rem default
+    rewind: true,
     speed: 500, //transition in milliseconds
     fixedWidth: '10vw',
-    // width: '100vw',
+    width: '36rem',
     perPage: 3,
-    start: 3,
-    perMove: 2,
+    start: 1,
+    perMove: 1,
     autoplay: true,
     interval: 3000,
     pagination: false,
